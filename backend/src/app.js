b06f7e238a94f1047b4bf86a580b8dacf3c3f983
ajkouva <ajkouva@gamil.com>
2026-02-18 20:26:06 +0530

    connected basic login

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const teacherLimiter = require('./middleware/teacher.limiter');
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
+    origin: true,//["http://localhost:5500", "http://localhost:5501", "http://127.0.0.1:5501"],
     credentials: true
 }));
 app.use(express.json({ limit: "10kb" }));
--- frontend/loginPage/script.js
+++ frontend/loginPage/script.js
@@ -1,26 +1,23 @@
 
-const EmailE1=document.querySelector(".Email");
-const PasswordE1=document.querySelector(".Password");
-const loginBtn=document.querySelector("#login-btn");
-loginBtn.addEventListener("click",(e)=>{
+const EmailE1 = document.querySelector(".Email");
+const PasswordE1 = document.querySelector(".Password");
+const loginBtn = document.querySelector("#login-btn");
+loginBtn.addEventListener("click", (e) => {
     loginwork();
 })
-async function loginwork(){
-    const Email=EmailE1.value;
-    const Password=PasswordE1.value;
+async function loginwork() {
+    const email = EmailE1.value;
+    const password = PasswordE1.value;
     try {
-        const rse = await fetch("http://localhost:3000/api/auth/login",{
-            "Email":Email,
-            "Password":Password
-        },{
-            method:"POST",
-             headers: {
-                "Content-Type": "application/json",
-        }})
+        const rse = await fetch("http://localhost:3000/api/auth/login", {
+            method: "POST",
+            credentials: 'include',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password })
+        })
         const data = await rse.json();
         console.log(data);
     } catch (error) {
-        
+        console.log(error);
     }
-     
 }
